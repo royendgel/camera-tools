ab2b23eae0164d56a1357b1a0d5ca36ea442fddb
Royendgel Silberie <royendgel@gmail.com>
2016-01-04 01:21:56 -0400

    handler implimentation

--- cameratools/__init__.py
+++ cameratools/__init__.py
@@ -1,4 +1,4 @@
-from time import sleep
+from time import sleep, time
 import datetime
 import os
 
@@ -10,8 +10,12 @@ class CameraTools(object):
     def __init__(self, ffmpeg_path='ffmpeg', recording_dir=''):
         self.recording_dir = recording_dir
         self.ffmpeg_path = ffmpeg_path
+        self.restart_on_error = True
+        self.start_time = None
+        self.last_time = None
+        self.elapsed = None
 
-    def pull_rtsp(self, url=None, out_name='video.mp4', duration=0):
+    def pull_rtsp(self, url=None, out_name='video.mp4', duration=0, handler=None):
         """
         Pull rtsp stream from camera
         :return:
@@ -21,6 +25,7 @@ class CameraTools(object):
         # cmd = "%s -y -probesize 32 -analyzeduration 0 -i %s %s" % (settings.FFMPEG_PATH, generate_cam_url(camera), out_name)
         rpath = os.path.join(self.recording_dir, out_name)
         cmd = "%s -y -i %s -vcodec copy -an %s" % (self.ffmpeg_path, url, rpath)
+        self.out_name = out_name
 
         thread = pexpect.spawn(cmd)
         cpl = thread.compile_pattern_list([
@@ -29,24 +34,37 @@ class CameraTools(object):
             "(.+)"
         ])
         start_time = datetime.datetime.now()
-        try:
-            while True:
+        while True:
+            try:
                 i = thread.expect_list(cpl, timeout=None)
-                end_time = datetime.datetime.now()
 
                 if i == 0:  # EOF
                     logging.info("the sub process exited")
                     break
                 elif i == 1:
-                    print end_time - start_time
+                    # When we get frame initialize start time
+                    if not self.start_time:
+                        self.start_time = time()
                     frame_number = thread.match.group(1)
                     size = int(thread.match.group(2)) * 1000
                     duration = thread.match.group(3)
                     logging.debug("Frame number %s" % (frame_number))
 
-        except:
-            self.close_thread(thread)
-            self.moov_atom(out_name)
+                self.handler(thread, i)
+
+            except KeyboardInterrupt:  # Break whenever you interupt via keyboard
+                self.close_thread(thread)
+                break
+
+
+            except Exception, e:  # on error call the on_error method
+                self.on_error(thread)
+
+            finally:  # maybe set error indication here ?
+                self.last_time = time()
+                if self.start_time:
+                    self.elapsed = self.last_time - self.start_time
+                self.handler(thread, i)
 
     def close_thread(self, thread):
         thread.sendintr()
@@ -54,13 +72,14 @@ class CameraTools(object):
             # if the thread is still alive, sleep for 1 second
             logging.info("Alive sleeping...")
             sleep(1)
+        self.moov_atom(self.out_name)
 
     def moov_atom(self, fname):
         fname = os.path.join(self.recording_dir, fname)
         temp = "%s_temp.mp4" % (fname)
         cmd = "%s -y -i %s -movflags +faststart %s" % (self.ffmpeg_path, fname, temp)
         thread = pexpect.run(cmd)
-        logging.info("did Mov Atom on the file: %s output : %s" % (temp, str(thread)))
+        logging.info("did Moov Atom on the file: %s output : %s" % (temp, str(thread)))
         os.remove(fname)
         logging.info("removed the file %s" % (fname))
         os.rename(temp, fname)
@@ -68,3 +87,10 @@ class CameraTools(object):
 
     def check_stream_active(self, type='rtsp'):
         pass
+
+    def handler(self, thread, i):
+        pass
+
+    def on_error(self, thread):
+        if not self.restart_on_error:
+            self.close_thread(thread)
